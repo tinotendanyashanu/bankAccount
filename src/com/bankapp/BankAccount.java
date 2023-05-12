ef1459d26fe92d738d7b278ff25aa63402b76dab
tinotendnyashanu <tinotendnyash@gmail.com>
2023-05-12 17:52:18 +0200

    constructor added

--- src/com/bankapp/BankAccount.java
+++ src/com/bankapp/BankAccount.java
@@ -7,6 +7,14 @@ public class BankAccount {
     private String email;
     private int phoneNumber;
 
+    public BankAccount(int accountNumber, double balance, String customerName, String email, int phoneNumber) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
     public int getAccountNumber() {
         return accountNumber;
     }
--- src/com/bankapp/Main.java
+++ src/com/bankapp/Main.java
@@ -3,7 +3,7 @@ package com.bankapp;
 public class Main {
     public static void  main(String[] args){
 
-        BankAccount tinotenda = new BankAccount();
+        BankAccount tinotenda = new BankAccount(12222,0.00,"Tinotenda Nyashanu","tinotendanyash@gmail.com",785428900);
         tinotenda.withdraw(100);
     }
 }
